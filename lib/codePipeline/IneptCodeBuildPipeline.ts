b7b9895cbf9a4b5485c402df9aa3b6f240d8261b
devCroaker <dev.croaker@gmail.com>
2021-07-06 00:29:31 -0700

    Add branch to code pipeline

--- lib/codePipeline/IneptCodeBuildPipeline.ts
+++ lib/codePipeline/IneptCodeBuildPipeline.ts
@@ -3,7 +3,7 @@ import { BuildSpec, PipelineProject } from '@aws-cdk/aws-codebuild'
 import { Artifact, Pipeline } from '@aws-cdk/aws-codepipeline'
 import { CodeBuildAction, GitHubSourceAction, GitHubTrigger, S3DeployAction } from '@aws-cdk/aws-codepipeline-actions'
 import { Bucket } from '@aws-cdk/aws-s3'
-import { GITHUB_OWNER, GITHUB_TOKEN, WEBSITE_REPO } from '../config/config'
+import { GITHUB_BRANCH, GITHUB_OWNER, GITHUB_TOKEN, WEBSITE_REPO } from '../config/config'
 import { StageDetails } from '../config/PipelineStages'
 
 export interface CodeBuildPipelineProps {
@@ -37,6 +37,7 @@ export class IneptCodeBuildPipeline extends Construct {
                 actionName: 'Checkout',
                 owner: GITHUB_OWNER,
                 repo: WEBSITE_REPO,
+                branch: GITHUB_BRANCH,
                 oauthToken: SecretValue.secretsManager(GITHUB_TOKEN),
                 output: outputSources,
                 trigger: GitHubTrigger.WEBHOOK
